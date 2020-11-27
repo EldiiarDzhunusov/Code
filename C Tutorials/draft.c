71a43c6ed2aefdc15ec97e074c21ea2287e43989
EldiiarDzhunusov <junusov.eldiyar@gmail.com>
2020-11-27 12:16:44 +0600

    Update

--- C Tutorials/draft.c
+++ C Tutorials/draft.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
-
-int isprime(int n){
-	int i;
-	for(i=2; i<n; i++){
-		if(n%i==0){
-			return 0;
-		}
-	}
-	return 1;
+#ifndef lli
+	#define lli long long int
+#endif
+int main ()
+{
+   lli number = 10;
+   printf("%d\n",number);
+   return 0;
 }
-
-int prime(int n){
-	int ans=3;
-	int counter = 1;
-	while(counter!=n){
-		ans++;
-		if(isprime(ans)==1){
-			counter++;
-		}
-		
-	}
-	return ans;
-}
-
-int main() {
-	int answer = prime(7);
-	printf("%d",answer);
-}
-
